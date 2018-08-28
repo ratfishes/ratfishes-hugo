e02107aab92af58a02841cf242a699a36d8c3bd8
ratfishes <ratfishes4@gmail.com>
2018-08-28 01:35:21 -0400

    test new config,no webpack etc, just hugo

--- src/js/cms-preview-templates/post.js
+++ src/js/cms-preview-templates/post.js
@@ -6,21 +6,11 @@ export default class PostPreview extends React.Component {
     const {entry, widgetFor, getAsset} = this.props;
     let image = getAsset(entry.getIn(["data", "image"]));
 
-    return <div className="mw6 center ph3 pv4">
-      <h1 className="f2 lh-title b mb3">{ entry.getIn(["data", "title"])}</h1>
-      <div className="flex justify-between grey-3">
-        <div style={{
-          width: "80px",
-          height: "80px"
-        }}></div>
-        <p>{ format(entry.getIn(["data", "date"]), "ddd, MMM D, YYYY") }</p>
-        <p>Read in x minutes</p>
-      </div>
-      <div className="cms mw6">
-        <p>{ entry.getIn(["data", "description"]) }</p>
-        { image && <img src={ image } alt={ entry.getIn(["data", "title"])} /> }
-        { widgetFor("body") }
-      </div>
-    </div>;
+    return <article class="post">
+    <h2 class="post-header">{ entry.getIn(["data", "title"])}</h2>
+    <p class="post-meta">{ format(entry.getIn(["data", "date"]), "YYYY-MM-DD") }</p>
+    { widgetFor("body") }
+  </article>;
+
   }
 }
